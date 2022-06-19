023849cf14722f72d5eea5addd0f1b43271e780e
Minteckk <s10228079@connect.np.edu.sg>
2022-06-19 11:10:00 +0800

    add onClickListener

--- app/src/main/java/sg/edu/np/mad/madassignment/StudentLogin.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/StudentLogin.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.madassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentLogin extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class StudentLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+        // find the id of student login button
+        Button student_login = findViewById(R.id.button);
+        student_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StudentLogin.this, StudentMain.class);
+                startActivity(intent);
+            }
+        });
     }
 }
