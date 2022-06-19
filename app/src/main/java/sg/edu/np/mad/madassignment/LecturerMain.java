f92cde54ef0e8d1b873c63106afb311fe5685aba
JunxiLu <S10219098@connect.np.edu.sg>
2022-06-19 15:57:37 +0800

    Finish lecturer page logout button

--- app/src/main/java/sg/edu/np/mad/madassignment/LecturerMain.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/LecturerMain.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class LecturerMain extends AppCompatActivity {
@@ -39,6 +40,16 @@ public class LecturerMain extends AppCompatActivity {
             }
         });
 
+        // set the onClick listener for the logout button
+        Button btn = findViewById(R.id.button_logout);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LecturerMain.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
         // find the id of the view student image
         ImageView viewStudent = findViewById(R.id.studentView);
         // set the onClick listener for the image
@@ -49,6 +60,7 @@ public class LecturerMain extends AppCompatActivity {
                 Intent viewIntent = new Intent(LecturerMain.this, ViewAllStudents.class);
                 startActivity(viewIntent);
             }
+
         });
     }
 }
