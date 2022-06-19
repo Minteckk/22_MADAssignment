2f070b1d47135d62a7846346d695f715513234cd
Minteckk <s10228079@connect.np.edu.sg>
2022-06-19 14:58:28 +0800

    Added studentProfile page

---
+++ app/src/main/java/sg/edu/np/mad/madassignment/StudentProfile.java
@@ -0,0 +1,47 @@
+package sg.edu.np.mad.madassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class StudentProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_profile);
+        // get the intent
+        Intent i = getIntent();
+        // receiving the intent
+        String name = i.getStringExtra("name");
+        String StudentID = i.getStringExtra("studentID");
+        // find the textview
+        TextView n = findViewById(R.id.textView18);
+        // set the textview text as StudentName
+        n.setText(name);
+        // find the textview
+        TextView id = findViewById(R.id.textView19);
+        // set the textview text as StudentID
+        id.setText(StudentID);
+        // find the id for the feedback button
+        Button feedbackBtn = findViewById(R.id.ProvideFeedback);
+        // set OnClick listener for the feedback button
+        feedbackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to the provide feedback page
+                Intent feedbackIntent = new Intent(StudentProfile.this, ProvideFeedback.class);
+                // send the intent to ProvideFeedback class
+                feedbackIntent.putExtra("name", name);
+                feedbackIntent.putExtra("studentID", StudentID);
+                // start the activity
+                startActivity(feedbackIntent);
+            }
+        });
+    }
+}
