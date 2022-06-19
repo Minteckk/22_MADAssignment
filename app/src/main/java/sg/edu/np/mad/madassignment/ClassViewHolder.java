80ac113bd87f5393e016deb654189e37b44e8ce6
Minteckk <s10228079@connect.np.edu.sg>
2022-06-19 14:42:19 +0800

    Add imageview Profile variable for alert dialog

--- app/src/main/java/sg/edu/np/mad/madassignment/ClassViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/ClassViewHolder.java
@@ -6,17 +6,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ClassViewHolder extends RecyclerView.ViewHolder {
+    // variable for studentName textview
     TextView sName;
+    // variable for studentID textview
     TextView sID;
+    // variable for profile textview
+    ImageView profile;
 
     public ClassViewHolder( View ViewItem) {
         super(ViewItem);
+        // find the id for studentName
         sName = ViewItem.findViewById(R.id.sName);
+        // find the id for studentId
         sID = ViewItem.findViewById(R.id.sID);
+        // find the id for studentProfile
+        profile = ViewItem.findViewById(R.id.profile);
     }
-
-
 }
