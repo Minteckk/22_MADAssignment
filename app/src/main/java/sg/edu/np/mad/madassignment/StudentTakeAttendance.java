2c63d53aa672fb7337e08dabdb97832788ee94a3
JunxiLu <S10219098@connect.np.edu.sg>
2022-06-20 20:27:43 +0800

    function for student take attendance

--- app/src/main/java/sg/edu/np/mad/madassignment/StudentTakeAttendance.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/StudentTakeAttendance.java
@@ -2,7 +2,15 @@ package sg.edu.np.mad.madassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class StudentTakeAttendance extends AppCompatActivity {
 
@@ -10,5 +18,23 @@ public class StudentTakeAttendance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_take_attendance);
+
+    }
+
+
+    public void compare(View view) {
+
+        String l_passcode = ((TextView) findViewById(R.id.generated_pc)).getText().toString();
+        String s_passcode = ((EditText) findViewById(R.id.student_acode)).getText().toString();
+        String value = ((EditText) findViewById(R.id.student_aid)).getText().toString();
+        int s_id=Integer.parseInt(value);
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+
+        if (s_passcode.equals(l_passcode)) {
+            boolean i = true;
+            AccountDBHandler.updateAttendance(s_id, date, i);
+        } else {
+
+        }
     }
 }
