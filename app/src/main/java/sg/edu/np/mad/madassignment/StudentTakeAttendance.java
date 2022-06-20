29811ed2286fc13decb27cded8a51f1692a48966
Minteckk <s10228079@connect.np.edu.sg>
2022-06-20 20:08:31 +0800

    remove sharedPreferences

--- app/src/main/java/sg/edu/np/mad/madassignment/StudentTakeAttendance.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/StudentTakeAttendance.java
@@ -2,7 +2,9 @@ package sg.edu.np.mad.madassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StudentTakeAttendance extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class StudentTakeAttendance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_take_attendance);
+
+
     }
 }
