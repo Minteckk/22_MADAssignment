68c30c1692a66d5eb656a1432a5b4e6c43adf1c0
Minteckk <s10228079@connect.np.edu.sg>
2022-06-20 18:50:35 +0800

    modify constructor

--- app/src/main/java/sg/edu/np/mad/madassignment/AccountDBHandler.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/AccountDBHandler.java
@@ -42,9 +42,9 @@ public class AccountDBHandler extends SQLiteOpenHelper {
     public static final String COLUMN_DATE = "date";
     public static final String COLUMN_FEEDBACK = "feedback";
 
-    public AccountDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
+    public AccountDBHandler(Context context)
     {
-        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
