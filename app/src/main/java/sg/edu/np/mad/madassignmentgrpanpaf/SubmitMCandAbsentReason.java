9477ff640f66c62a6fc050c29731c36df63fb4a7
Minteckk <s10228079@connect.np.edu.sg>
2022-07-13 17:04:35 +0800

    testing

---
+++ app/src/main/java/sg/edu/np/mad/madassignmentgrpanpaf/SubmitMCandAbsentReason.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.madassignmentgrpanpaf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmitMCandAbsentReason extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_mcand_absent_reason);
+    }
+}
