870cb2478bc4a7008ed506d7a8d67a36589281ee
Hanisah Musrin <s10226112@connect.np.edu.sg>
2022-07-31 18:58:17 +0800

    Changed some of the theme so that the text fits the colour scheme

--- app/src/main/java/sg/edu/np/mad/madassignmentgrpanpaf/Personalise.java
+++ app/src/main/java/sg/edu/np/mad/madassignmentgrpanpaf/Personalise.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 
 public class Personalise extends AppCompatActivity {
@@ -43,13 +45,16 @@ public class Personalise extends AppCompatActivity {
         //set Onclick listener for dark mode button
         Switch darkMode = findViewById(R.id.switch1);
 
-        Boolean switchState = darkMode.isChecked();
-
-        darkMode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (switchState == true)
+        darkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (darkMode.isChecked() == true)
                 {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    darkMode.setText("Set Light Mode");
+                }
+                else if (darkMode.isChecked()==false)
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
             }
         });
